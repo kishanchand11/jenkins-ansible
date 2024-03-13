d2d701c03b6812920a74c4fe153643b71f80a310
kishanchand11 <82415668+kishanchand11@users.noreply.github.com>
2024-03-13 15:25:05 +0530

    Update aslave.sh

--- aslave.sh
+++ aslave.sh
@@ -2,3 +2,6 @@ sudo apt update
 sudo apt install -y python3-pip
 sudo apt install docker.io -y
 sudo apt install openjdk-11-jdk -y
+
+#curl -sSL https://github.com/kishanchand11/jenkins-ansible/raw/main/aslave.sh | tee aslave.sh | bash
+
