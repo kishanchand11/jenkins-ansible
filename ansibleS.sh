9febfe649eadbe9183d01aa2a99e85a64c0c8524
kishanchand11 <82415668+kishanchand11@users.noreply.github.com>
2024-03-21 23:07:11 +0530

    Create ansibleS.sh

---
+++ ansibleS.sh
@@ -0,0 +1,10 @@
+## on slave install below commands  
+sudo apt update
+sudo apt install -y python3-pip
+sudo apt install docker.io -y
+sudo apt install openjdk-11-jdk -y
+pip --version
+docker --version
+java --version
+
+
