043334dbd5ac526298f6d12ea263f8acb9351fce
kishanchand11 <82415668+kishanchand11@users.noreply.github.com>
2024-03-21 23:25:12 +0530

    Delete aslave.sh

--- aslave.sh
+++
@@ -1,7 +0,0 @@
-sudo apt update
-sudo apt install -y python3-pip
-sudo apt install docker.io -y
-sudo apt install openjdk-11-jdk -y
-
-#curl -sSL https://github.com/kishanchand11/jenkins-ansible/raw/main/aslave.sh | tee aslave.sh | bash
-
