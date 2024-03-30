a2a619ea8ff6a7a5be5c8deadd7c53b3f852cd38
kishanchand11 <82415668+kishanchand11@users.noreply.github.com>
2024-03-30 11:08:24 +0530

    Update ansibleM.sh

--- ansibleM.sh
+++ ansibleM.sh
@@ -1,12 +1,16 @@
 ### on master paste these commands
 
 sudo apt update
+
 sudo apt install software-properties-common
+
 sudo apt-add-repository --yes --update ppa:ansible/ansible
+
 sudo apt install ansible -y
+
 ansible --version
 
 
-#curl -sSL https://github.com/kishanchand11/jenkins-ansible/raw/master/ansibleM.sh | tee ansibleM.sh | sudo bash
+#curl -sSL https://github.com/kishanchand11/jenkins-ansible/raw/main/ansibleM.sh | tee ansibleM.sh | sudo bash
 #or
-#curl -sSL https://github.com/kishanchand11/jenkins-ansible/master/ansibleM.sh | sudo tee ansibleM.sh >/dev/null && sudo bash ansibleM.sh
+#curl -sSL https://github.com/kishanchand11/jenkins-ansible/main/ansibleM.sh | sudo tee ansibleM.sh >/dev/null && sudo bash ansibleM.sh
