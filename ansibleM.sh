5f3932c7386821f772e3c2c785286b7ba2988fa1
kishanchand11 <82415668+kishanchand11@users.noreply.github.com>
2024-05-02 16:32:27 +0530

    Update ansibleM.sh

--- ansibleM.sh
+++ ansibleM.sh
@@ -11,6 +11,6 @@ sudo apt install ansible -y
 ansible --version
 
 
-#curl -sSL https://github.com/kishanchand11/jenkins-ansible/raw/main/ansibleM.sh | tee ansibleM.sh | sudo bash
+#curl -sSL https://github.com/kishanchand11/jenkins-ansible/raw/main/ansibleM.sh | sudo tee ansibleM.sh | sudo bash
 #or
 #curl -sSL https://github.com/kishanchand11/jenkins-ansible/main/ansibleM.sh | sudo tee ansibleM.sh >/dev/null && sudo bash ansibleM.sh
