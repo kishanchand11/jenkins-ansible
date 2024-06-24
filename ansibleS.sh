26b21ac504608945a7b5160b11a41ce017a6e87b
kishanchand11 <82415668+kishanchand11@users.noreply.github.com>
2024-06-24 13:52:00 +0530

    Update ansibleS.sh

--- ansibleS.sh
+++ ansibleS.sh
@@ -1,7 +1,6 @@
 ## on slave install below commands  
 sudo apt update
 sudo apt install -y python3-pip
-sudo apt install docker.io -y
 sudo apt install openjdk-11-jdk -y
 pip --version
 docker --version
